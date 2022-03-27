910664af3d18c787069842d329adefba7fad20f4
Emily Daykin <emilydaykin31@gmail.com>
2022-03-27 15:12:45 +0200

    updated index.js and package jsons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/style.scss';
+import './styles/navbar.scss';
+import './styles/home.scss';
+import './styles/about.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
