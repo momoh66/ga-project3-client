5f77701035d7fddd8ce4331a15d6920257e44ddd
Emily Daykin <emilydaykin31@gmail.com>
2022-03-28 10:29:16 +0200

    updated index.js and index.html

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import './styles/style.scss';
 import './styles/navbar.scss';
 import './styles/home.scss';
 import './styles/about.scss';
+import './styles/neighbourhoods.scss';
+import './styles/login.scss';
+import './styles/register.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
