0fb94288ad4a883f4d738f3a90aa67fd827b8203
Emily Daykin <emilydaykin31@gmail.com>
2022-03-29 16:08:49 +0200

    Styling fixes to nav and sidebars affecting height of every page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/navbar.scss';
 import './styles/home.scss';
 import './styles/about.scss';
 import './styles/neighbourhoods.scss';
+import './styles/single-profile.scss';
 import './styles/login.scss';
 import './styles/register.scss';
 
